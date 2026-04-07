1b2a94b1a6211939efd08da7e41fc121ba083008
Mayurjoshi00 <joshimayur@outlook.in>
2026-04-08 01:28:33 +0530

    photo appears and other bugs

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 
 // ---------- MIDDLEWARE ----------
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // ---------- SERVE FRONTEND ----------
 app.use(express.static(path.join(__dirname, "../frontend")));
